1fe908b8887b068bce8d0ee1cc8a4b1d9fe6ff42
2115101056 <114454099+2115101056@users.noreply.github.com>
2022-11-11 10:19:13 +0800

    Add files via upload

---
+++ JF Java Fundamentals Learner - Oracle/Section 3 - GreenFoot/JF 3-3 Source Code and Documentation/3-3 Project/JF_V02_503_L01PrjStudent/MyWorld.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * Ngakan Gde Satria Abirama_2115101056
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1);
+        
+        
+    }
+}
